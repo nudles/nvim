287a054fec0dba99644f7a121072e560116f007b
fredwang <fredwang@bytedance.com>
2024-01-17 05:18:09 +0000

    stop add headers for cpp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -22,7 +22,6 @@ lspconfig.clangd.setup {
       "clangd",
       "--background-index",
       "--clang-tidy",
-      "--header-insertion=iwyu",
       "--completion-style=detailed",
       "--function-arg-placeholders",
       "--fallback-style=llvm",
