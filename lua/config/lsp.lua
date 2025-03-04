609bdfbd93378f6d009b1e84ba51ac6022b5ca8e
fredwang <fredwang@bytedance.com>
2025-03-04 10:17:05 +0800

    update lsp config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,12 +8,14 @@ local lspconfig = require("lspconfig")
 lspconfig.clangd.setup({
   cmd = { "clangd", 
     "--background-index",        -- Enable background indexing
-    "--clang-tidy=false",        -- Disable extra linting (makes it faster)
+    "--clang-tidy=true",        -- Disable extra linting (makes it faster)
     "--completion-style=detailed", -- Use detailed autocompletion
     "--cross-file-rename",       -- Enable renaming across files
     "--limit-results=100",       -- Limit number of results for completion
     "--header-insertion=never",  -- Don't auto-add headers (reduces disk I/O)
     "--pch-storage=memory",      -- Store precompiled headers in memory
+    "-j=12",
+    "--query-driver=clang++",
   },
   filetypes = { "c", "cpp", },
   root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
